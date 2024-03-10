11a2b85674ee6f75cc856512670b7ceb5777fdcf
Fabrizio Giordano <fabrizio.giordano@ext.esa.int>
2024-03-10 16:06:09 +0100

    New set of icons: couple missing

--- src/js/modules/controlpanel/ControlPanelView.js
+++ src/js/modules/controlpanel/ControlPanelView.js
@@ -14,20 +14,38 @@ class ControlPanelView {
 			"	<div id='hamburgerButton'>" +
 			"		<span></span><span></span><span></span><span></span>" +
 			"	</div>" +
-			"	<img id='gotoButton' class='controlButton' src='images/catbutton2.png'/>" +
+			"	<img id='gotoButton' class='controlButton' src='images/new_search.png'/>" +
 			"	<div id='gotoButtonText'>Search</div>" +
-			"	<img id='cataloguesButton' class='controlButton' src='images/catbutton2.png' />" +
+			"	<img id='cataloguesButton' class='controlButton' src='images/new_catalogue.png' />" +
 			"	<div id='catalogueButtonText'>Overlay source catalogues on top of a map</div>" +
-			"	<img id='footprintsButton' class='controlButton' src='images/imgbutton2.png' />" +
+			"	<img id='footprintsButton' class='controlButton' src='images/new_footprints.png' />" +
 			"	<div id='footprintButtonText'>Overlay observation footprints on top of a map</div>" +
 			"	<img id='mapsButton' class='controlButton' src='images/mapbutton2.png' />" +
 			"	<div id='mapsButtonText'>Sky map selection</div>" +
-			"	<img id='settingsButton' class='controlButton' src='images/settings.png' />" +
+			"	<img id='settingsButton' class='controlButton' src='images/new_settings.png' />" +
 			"	<div id='settingsButtonText'>Settings</div>" +
 			"	<img id='cutoutButton' class='controlButton' src='images/settings.png' />" +
 			"	<div id='cutoutButtonText'>Data Explorer</div>" +
 			"</div>" +
 			"<div id='settings-popup'></div>";
+		// this._html = "<div id='controlButtonContainer'>" +
+		// 	"	<div id='hamburgerButton'>" +
+		// 	"		<span></span><span></span><span></span><span></span>" +
+		// 	"	</div>" +
+		// 	"	<img id='gotoButton' class='controlButton' src='images/catbutton2.png'/>" +
+		// 	"	<div id='gotoButtonText'>Search</div>" +
+		// 	"	<img id='cataloguesButton' class='controlButton' src='images/catbutton2.png' />" +
+		// 	"	<div id='catalogueButtonText'>Overlay source catalogues on top of a map</div>" +
+		// 	"	<img id='footprintsButton' class='controlButton' src='images/imgbutton2.png' />" +
+		// 	"	<div id='footprintButtonText'>Overlay observation footprints on top of a map</div>" +
+		// 	"	<img id='mapsButton' class='controlButton' src='images/mapbutton2.png' />" +
+		// 	"	<div id='mapsButtonText'>Sky map selection</div>" +
+		// 	"	<img id='settingsButton' class='controlButton' src='images/settings.png' />" +
+		// 	"	<div id='settingsButtonText'>Settings</div>" +
+		// 	"	<img id='cutoutButton' class='controlButton' src='images/settings.png' />" +
+		// 	"	<div id='cutoutButtonText'>Data Explorer</div>" +
+		// 	"</div>" +
+		// 	"<div id='settings-popup'></div>";
 		
 	}
 
